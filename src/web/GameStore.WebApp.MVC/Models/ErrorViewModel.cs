d5e337b9008c167cb661f025d3c01cc2b0563648
rodrigo.figueira@sorteonline.com.br <rodrigo.figueira@sorteonline.com.br>
2020-11-28 15:11:33 -0300

    Add project files.

---
+++ src/web/GameStore.WebApp.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GameStore.WebApp.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
