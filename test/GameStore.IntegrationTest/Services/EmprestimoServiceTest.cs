921dafe14f19db12710149848f34806e6da97ed4
Rodrigo Figueira <rodrigo.figueiraufg@gmail.com>
2020-12-06 21:40:10 -0300

    Adicionando Tests de Serviços.

---
+++ test/GameStore.IntegrationTest/Services/EmprestimoServiceTest.cs
@@ -0,0 +1,60 @@
+﻿using GameStore.Domain.Interfaces;
+using GameStore.Domain.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+
+namespace GameStore.IntegrationTest.Services
+{
+    [TestClass()]
+    public class EmprestimoServiceTest
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            var mock = new Mock<IEmprestimoService>();
+            var emprestimo = new Emprestimo
+            {
+                Jogo = JogoCreate(),
+                Amigo = AmigoCreate()
+            };
+            mock.Setup(e => e.Adicionar(emprestimo));
+            mock.Object.Adicionar(emprestimo);
+        }
+
+        [TestMethod()]
+        public void UpdateByIdTest()
+        {
+            var mock = new Mock<IEmprestimoService>();
+            var emprestimo = new Emprestimo
+            {
+                Jogo = JogoCreate(),
+                Amigo = AmigoCreate()
+            };
+            mock.Setup(e => e.Atualizar(emprestimo));
+            mock.Object.Atualizar(emprestimo);
+        }
+
+        [TestMethod()]
+        public void DeleteByIdTest()
+        {
+            var mock = new Mock<IEmprestimoService>();
+            var guid = Guid.NewGuid();
+            mock.Setup(e => e.Remover(guid));
+            mock.Object.Remover(guid);
+        }
+        public Jogo JogoCreate()
+        {
+            Jogo jogo = new Jogo("Ratchet & Clank",
+                                 "Ratchet & Clank é o ponto culminante de tudo o que a Insomniac fez com a clássica franquia nos últimos 14 anos.",
+                                 false);
+            return jogo;
+        }
+
+        public Amigo AmigoCreate()
+        {
+            Amigo amigo = new Amigo("Zé Rodrigo", "zezinho", "ze.rodrigo@gmail.com");
+            return amigo;
+        }
+    }
+}
---
+++ test/GameStore.IntegrationTest/Services/JogoServiceTests.cs
@@ -0,0 +1,50 @@
+﻿using GameStore.Domain.Interfaces;
+using GameStore.Domain.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+
+namespace GameStore.IntegrationTest.Services
+{
+    [TestClass()]
+    public class JogoServiceTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            var mock = new Mock<IJogoService>();
+            var jogoViewModel = new Jogo
+            {
+                Nome = "Jogo 1",
+                Genero = "Guerra",
+                Emprestado = false,
+            };
+            mock.Setup(e => e.Adicionar(jogoViewModel));
+            mock.Object.Adicionar(jogoViewModel);
+        }
+
+        [TestMethod()]
+        public void UpdateByIdTest()
+        {
+            var mock = new Mock<IJogoService>();
+            var guid = Guid.NewGuid();
+            var jogoViewModel = new Jogo
+            {
+                Nome = "Jogo 1",
+                Genero = "Guerra",
+                Emprestado = false,
+            };
+            mock.Setup(e => e.Atualizar(jogoViewModel));
+            mock.Object.Atualizar(jogoViewModel);
+        }
+
+        [TestMethod()]
+        public void DeleteByIdTest()
+        {
+            var mock = new Mock<IJogoService>();
+            var guid = Guid.NewGuid();
+            mock.Setup(e => e.Remover(guid));
+            mock.Object.Remover(guid);
+        }
+    }
+}
