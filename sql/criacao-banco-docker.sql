04444731ce4323cb7daef2d660b580692634b832
Rodrigo Figueira <rodrigo.figueiraufg@gmail.com>
2020-12-08 08:53:06 -0300

    Ajuste script de carga

--- sql/criacao-banco-docker.sql
+++ sql/criacao-banco-docker.sql
@@ -188,6 +188,20 @@ CREATE TABLE [dbo].[Jogos](
 	[Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
+INSERT [dbo].[Amigos] ([Id], [Nome], [Apelido], [Email]) VALUES (N'a22d7a31-8a05-439d-7201-08d89b653cb5', N'Rodrigo Martins', N'Crispin', N'rodrigo.martins@gmail.com')
+INSERT [dbo].[Amigos] ([Id], [Nome], [Apelido], [Email]) VALUES (N'39a7be57-e067-4ec8-7202-08d89b653cb5', N'Felipe Soares', N'Pequeno', N'felipe.soares@gmail.com')
+GO
+INSERT [dbo].[AspNetUsers] ([Id], [UserName], [NormalizedUserName], [Email], [NormalizedEmail], [EmailConfirmed], [PasswordHash], [SecurityStamp], [ConcurrencyStamp], [PhoneNumber], [PhoneNumberConfirmed], [TwoFactorEnabled], [LockoutEnd], [LockoutEnabled], [AccessFailedCount]) VALUES (N'f3acbf6e-7c3f-489f-a8a1-beb12fa2c9c1', N'admin@test.com.br', N'ADMIN@TEST.COM.BR', N'admin@test.com.br', N'ADMIN@TEST.COM.BR', 1, N'AQAAAAEAACcQAAAAEJO1EtI3A2m1UxaYmAIK8vcpdjABvOgnzn2kVLyf8sayggeZ6P8nx6RCczKs8wYhaw==', N'W4ZRJ7X7QRPAFRE4IOK2ZCAJ4KLWIXSL', N'9739a80a-9ec4-444b-8bb8-89bd91983c92', NULL, 0, 0, NULL, 1, 0)
+GO
+INSERT [dbo].[Emprestimos] ([Id], [AmigoId], [JogoId]) VALUES (N'47622442-2aa6-47c8-2511-08d89b653cbc', N'a22d7a31-8a05-439d-7201-08d89b653cb5', N'1d657050-f3cf-45e9-6ac1-08d89b6421d4')
+INSERT [dbo].[Emprestimos] ([Id], [AmigoId], [JogoId]) VALUES (N'00d5f2e1-53e1-4890-2512-08d89b653cbc', N'39a7be57-e067-4ec8-7202-08d89b653cb5', N'75d0648b-3b6d-4e83-6ac3-08d89b6421d4')
+GO
+INSERT [dbo].[Jogos] ([Id], [Nome], [Genero], [Emprestado]) VALUES (N'1d657050-f3cf-45e9-6ac1-08d89b6421d4', N'Final Fantasy VII Remake', N'JRPG', 1)
+INSERT [dbo].[Jogos] ([Id], [Nome], [Genero], [Emprestado]) VALUES (N'4b12cb5e-bc3c-48ba-6ac2-08d89b6421d4', N'Red Dead Redemption 2', N'Mundo aberto', 0)
+INSERT [dbo].[Jogos] ([Id], [Nome], [Genero], [Emprestado]) VALUES (N'75d0648b-3b6d-4e83-6ac3-08d89b6421d4', N'God of War', N'Terceira pessoa', 1)
+INSERT [dbo].[Jogos] ([Id], [Nome], [Genero], [Emprestado]) VALUES (N'1182ce6e-bad8-471e-6ac4-08d89b6421d4', N'Homem-Aranha da Marvel', N'Super-heróis', 0)
+INSERT [dbo].[Jogos] ([Id], [Nome], [Genero], [Emprestado]) VALUES (N'09137eed-7575-4802-6ac5-08d89b6421d4', N'Tony Hawk’s Pro Skater 1+2', N'remake ', 0)
+INSERT [dbo].[Jogos] ([Id], [Nome], [Genero], [Emprestado]) VALUES (N'474f2123-380c-4312-6ac6-08d89b6421d4', N'The Last of Us Parte II', N'Ação-aventura', 0)
 GO
 ALTER TABLE [dbo].[Emprestimos] ADD  DEFAULT ('00000000-0000-0000-0000-000000000000') FOR [AmigoId]
 GO
